b9bcc2feb227df8fa82fa494ed94c35c472da111
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2024-05-08 21:41:30 +0200

    project updates

--- MyAspire.AppHost/Program.cs
+++ MyAspire.AppHost/Program.cs
@@ -5,25 +5,29 @@ var redis = builder.AddRedis("redis", 5379)
     .WithVolume("redis_data", "/data", false)
     .WithOtlpExporter();
 
-var postgreSql = builder.AddPostgres("postgresql", port: 4432)
-    .WithEnvironment("POSTGRES_PASSWORD", "password")
-    .WithEnvironment("POSTGRES_USER", "postgres")
+var postgreSql = builder.AddPostgres("postgres")//, port: 4432)
+                                                //.WithEnvironment("POSTGRES_PASSWORD", "password")
+                                                //.WithEnvironment("POSTGRES_USER", "postgres")
     .WithImageTag("latest")
     .WithImage("postgres")
     .WithVolume("postgres_data", "/var/lib/postgresql/data")
-    .WithPgAdmin()
-    .WithOtlpExporter()
-    .WithContainerRunArgs();
+    .WithPgAdmin();
+//    .WithDataVolume();
+//    .WithOtlpExporter()
+//    .WithContainerRunArgs();
 
-var solace = builder.AddContainer("pubSubStandardSingleNode", "solace/solace-pubsub-standard", "latest")
-    .WithVolume("storage-group", "/var/lib/solace")
-    .WithContainerRunArgs("--shm-size","1g", "--ulimit","core=-1", "--ulimit","nofile=2448:6592")
-    .WithEnvironment("system_scaling_maxconnectioncount", "100")
-    .WithEnvironment("username_admin_password","admin")
-    .WithEnvironment("username_admin_globalaccesslevel","admin")
-    .WithHttpEndpoint(8080,8080, "admin");
+var db = postgreSql.AddDatabase("fx-rates", "fx-rates");
 
-var apiService = builder.AddProject<Projects.MyAspire_ApiService>("apiservice");
+//var solace = builder.AddContainer("pubSubStandardSingleNode", "solace/solace-pubsub-standard", "latest")
+//    .WithVolume("storage-group", "/var/lib/solace")
+//    .WithContainerRunArgs("--shm-size", "1g", "--ulimit", "core=-1", "--ulimit", "nofile=2448:6592")
+//    .WithEnvironment("system_scaling_maxconnectioncount", "100")
+//    .WithEnvironment("username_admin_password", "admin")
+//    .WithEnvironment("username_admin_globalaccesslevel", "admin")
+//    .WithHttpEndpoint(8080, 8080, "admin");
+
+var apiService = builder.AddProject<Projects.MyAspire_ApiService>("apiservice")
+    .WithReference(db);
 
 builder.AddProject<Projects.MyAspire_Web>("webfrontend")
     .WithReference(apiService);
